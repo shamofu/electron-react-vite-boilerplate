82d16d54fcec91316d0e2f55e7fd31ad90474c63
shamofu <17104318+shamofu@users.noreply.github.com>
2021-04-14 01:08:23 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   collectCoverageFrom: [
     "app/renderer/**",
+    "!app/renderer/vite.config.js",
     "!app/renderer/index.tsx",
     "!app/renderer/tests/**",
     "!app/renderer/dist/**"
